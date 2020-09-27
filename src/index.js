81e2e5e290bf04eee2dc159c706a9199b14976ad
Brian Cross <briancross@shaw.ca>
2020-09-27 16:40:16 -0600

    Get webcam stream and log to console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import adapter from "webrtc-adapter";
+
+// Get video / audio feed from webcam
+navigator.mediaDevices
+  .getUserMedia({ audio: true, video: true })
+  .then((stream) => console.log(stream))
+  .catch(console.log);
