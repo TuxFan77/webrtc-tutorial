daa199411d6dac5950984b46ace418a34ff7ace8
Brian Cross <briancross@shaw.ca>
2020-09-27 16:46:53 -0600

    Connect media stream object to the local video display in the DOM.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import adapter from "webrtc-adapter";
 // Get video / audio feed from webcam
 navigator.mediaDevices
   .getUserMedia({ audio: true, video: true })
-  .then((stream) => console.log(stream))
+  .then((stream) => {
+    console.log(stream);
+    document.querySelector(".local-video").srcObject = stream;
+  })
   .catch(console.log);
